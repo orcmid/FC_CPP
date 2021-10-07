28d786a5979972fee64649fb11e916b086ec9ed6
Dennis E. Hamilton <dennis.hamilton@acm.org>
2021-10-06 18:55:00 -0700

    0.0.4 hello.c Brought here
    Part of creating a GitHub project for the Udemy Fundamentals of C++ for Game Developers.

---
+++ S1E6-FirstProgram/hello.c
@@ -0,0 +1,24 @@
+/* hello.c 0.0.4                    UTF-8                      dh:2021-10-06
+*---|----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
+
+                     The "Your First Program" Exercise
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello, my name is Dennis");
+        // who is reporting
+    puts("My home town was Tacoma, Washington.");
+        // what is the home town
+    puts("My favorite hobby is playing cinematic games.");
+        // what is the favorite activity
+    }
+
+/* 0.0.4 switched from printf() to puts() for simplest and direct.
+   0.0.3 renamed hello.c and tidied up a little for orcmid/FP_CPP-GameDev
+   0.0.2 with comments
+   0.0.1 calling printf() two more times.
+   0.0.0 the simple printf
+   */
