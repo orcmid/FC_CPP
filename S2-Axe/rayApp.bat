ee784f271e5b46af05b07c387bed782cb663cf7f
Dennis E. Hamilton <dennis.hamilton@acm.org>
2021-11-09 19:33:30 -0800

    0.0.6 S2-axe\rayAll.bat Victory
    Everything there is to do until bundled in a VCrayall.zip release

--- S2-Axe/rayApp.bat
+++ S2-Axe/rayApp.bat
@@ -1,5 +1,5 @@
 @echo off
-rem VCrayApp 0.0.0 rayApp.bat 0.0.5 UTF-8                         2021-11-10
+rem VCrayApp 0.0.0 rayApp.bat 0.0.6 UTF-8                         2021-11-10
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 
 rem                  BUILDING RAYLIB APP WITH VC/C++ TOOLS
@@ -38,31 +38,36 @@ rem        into that of a calling script.
 rem     %2 might then be "*" and allow for that.
 rem don't shift anything out until %1-%2 handled.
 SET VCterse=
+SET VChush=
 SET VCsplice=%1
 IF NOT "%1" == "+" GOTO :MAYBETERSE
-IF "%2" == "*" SET VCterse=^>NUL
+IF NOT "%2" == "*" GOTO :MAYBETERSE
+SET VCterse=^>NUL 2^>NUL
+Set VChush=/nologo
 
 :MAYBETERSE
 rem SELECT TERSE OR VERBOSE
-IF "%1" == "*" SET VCterse=^>NUL
-rem                used to dump verbose echoes
+IF "%1" == "*" ( SET VCterse=^>NUL 2^>NUL
+                 SET VChush=/nologo
+                 )
 
 rem ANNOUNCE THIS SCRIPT
 IF "%1" == "*" GOTO :WHISPER
 IF "%1" == "+" GOTO :WHISPER
-TITLE BUILDING RAYLIB APP WITH VC/C++ TOOLS
 CLS
 
 :WHISPER
+rem CONFIRM COMMAND-LINE ENVIRONMENT
+IF "%VSCMD_VER%" == "" GOTO FAIL3
+WHERE cl.exe >nul 2>nul
+IF ERRORLEVEL 1 goto FAIL3
 ECHO: [rayApp] %VCrayApp% BUILDING RAYLIB APP WITH VC/C++ TOOLS
 
 IF "%VCsplice%" == "+" GOTO :LOCATE
 ECHO:          %TIME% %DATE% on %USERNAME%'s %COMPUTERNAME%         %VCterse%
 ECHO:          %~dp0                                                %VCterse%
-rem            reporting script directory location
 
 :LOCATE
-ECHO           %VCterse%
 rem VERIFY LOCATION OF THE SCRIPT WHERE VCRayApp.zip IS FULLY EXTRACTED
 rem Some are customizable, none should be removed, all %VCrayApp% specific
 IF NOT EXIST "%~dp0cache\cache.txt" GOTO :FAIL1
@@ -86,26 +91,20 @@ IF "%1" == "-r" ( SET VCrun=1
                   SHIFT /1 )
 IF NOT "%1" == "" GOTO FAIL 2
 
-rem CONFIRM COMMAND-LINE ENVIRONMENT
-IF "%VSCMD_VER%" == "" GOTO FAIL3
-WHERE /Q cl.exe >nul 2>nul
-IF ERRORLEVEL 1 goto FAIL3
-ECHO: [rayApp] Using Tools %VSCMD_VER% for %VSCMD_ARG_TGT_ARCH%     %VCterse%
-ECHO           %VCterse%
 
 rem COMPILE INTO THE CACHE IF NEEDED
 IF NOT "%VCclean%" == "1" GOTO :CACHECHECK
 DEL %~dp0cache\rglfw.obj
 
 :CACHECHECK
+SET VCfrom=%CD%
 IF EXIST %~dp0cache\rglfw.obj GOTO :APPBUILD
 DEL %~dp0cache\*.obj > nul 2>nul
 
-SET VCfrom=%CD%
 CD %~dp0cache
-CL.exe /w /c @VCoptions.opt @raylibVars.opt @raylibCode.opt         %VCterse%
+CL.exe %VChush% /w /c @VCoptions.opt @raylibVars.opt @raylibCode.opt %VCterse%
 IF ERRORLEVEL 2 GOTO :FAIL4
-ECHO: [rayApp] FRESH CACHE OF RAYLIB *.OBJ FILES COMPILED           %VCterse%
+ECHO: [rayApp] FRESH CACHE OF RAYLIB *.OBJ FILES COMPILED
 ECHO: %VCterse%
 
 :APPBUILD
@@ -113,28 +112,29 @@ CD %~dp0app
 DEL *.exe >nul 2>nul
 rem Flags
 SET OUTPUT_FLAG=/Fe: "%GAME_EXE%"
-SET SUBSYSTEM_FLAGS=/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup
+SET SUBSYS=/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup
 
 rem Compiling the %SRC%
-CL.exe /W3 /c @~dp0cache\VCoptions.opt %~dp0%SRC%                   %VCterse%
+CL.exe %VChush% /W3 /c @%~dp0cache\VCoptions.opt %~dp0%SRC%          %VCterse%
 IF ERRORLEVEL 2 goto :FAIL5
 ECHO: %VCterse%
 
 rem Linking it all to %GAME_EXE%
-CL.exe %OUTPUT_FLAG% @rayLinking.opt /link /LTCG %SUBSYSTEM_FLAGS%  %VCterse%
+CL.exe %VChush% %OUTPUT_FLAG% @rayLinking.opt /link /LTCG %SUBSYS% %VCterse%
 IF ERRORLEVEL 2 goto :FAIL5
 ECHO: %VCterse%
 DEL *.obj >nul 2>nul
-ECHO: [rayApp] RAYLIB APP %dp0app\%GAME_EXE% COMPILED               %VCterse%
+
+ECHO: [rayApp] RAYLIB APP %GAME_EXE% COMPILED TO APP FOLDER
 ECHO: %VCterse%
 
 CD %VCfrom%
 IF NOT "%VCrun%" == "1" GOTO :SUCCESS
-%dp0app\%GAME_EXE%
+"%~dp0app\%GAME_EXE%"
 
 :SUCCESS
+ENDLOCAL
 IF "%VCsplice%" == "+" EXIT /B 0
-IF "%VCterse%" == "" PAUSE
 EXIT /B 0
 
 :FAIL5
@@ -160,7 +160,7 @@ ECHO:          NO ACTIONS HAVE BEEN PERFORMED                       %VCterse%
 GOTO :BAIL
 
 :FAIL2
-ECHO: [rayApp] **** FAIL: UNSUPPORTED RAYAP PARAMETERS ****
+ECHO: [rayApp] **** FAIL: UNSUPPORTED RAYAPP.BAT PARAMETERS ****
 ECHO:          Invalid Here: %*
 ECHO:          %VCterse%
 ECHO:          NO ACTIONS HAVE BEEN PERFORMED                       %VCterse%
@@ -186,23 +186,21 @@ GOTO :BAIL
 
 :USAGE
 rem    PROVIDE USAGE INFORMATION
-ECHO:   %VCterse%
 ECHO:   USAGE: rayApp [+] ?
 ECHO:          rayApp [+] [*] [-c] [-r]
 IF NOT "%1" == "?" GOTO :BAIL
-ECHO:   where
-ECHO:           ? produces this usage information.
-ECHO:           + for operating non-stop without any screen clearings
-ECHO:             and pausings.  Good for providing output as a helper
-ECHO:             to a calling script.
-ECHO:           * selects terse output.  If operation fails, repeat
-ECHO:             the command line without this option for more details.
-ECHO:          -c for a complete rebuild of any cache before building the app
-ECHO:          -r for running the app on successful build
+ECHO:   where  ? produces this usage information.
+ECHO:          + for operating non-stop without any screen clearing
+ECHO:            and pausing.  Good for use called as a helper
+ECHO:          * selects terse output.  If operation fails, repeat
+ECHO:            without this option for more details.
+ECHO:         -c for a complete rebuild of any cache
+ECHO:         -r for running the app on successful build
 ECHO:
-ECHO:    Exit code 0 is produced on all successful operations.  Exit codes
-ECHO:    greater than 1 are produced for all failure cases.
+ECHO:    Exit code 0 is produced on all successful operations.
+ECHO:    Exit codes greater than 1 are produced for any failure.
 ECHO:
+ENDLOCAL
 IF "%VCsplice%" == "+" EXIT /B 0
 PAUSE
 EXIT /B 0
@@ -213,11 +211,13 @@ IF NOT ERRORLEVEL 2 SET ERRORLEVEL=2
 IF NOT "%VCterse%" == "" EXIT /B %ERRORLEVEL%
 IF "%VCsplice%" == "+" EXIT /B %ERRORLEVEL%
 ECHO:
+ENDLOCAL
 PAUSE
 EXIT /B %ERRORLEVEL%
 
 rem |----1----|----2----|----3----|----4----|----5----|----6----|----7----|--*
 rem
+rem 0.0.6 2021-11-10T03:30Z Confirmed and touched-up ready for nfoTools
 rem 0.0.5 2021-11-10T00:08Z Complete draft of guard-railed script
 rem 0.0.4 2021-11-08T23:43Z First stage provisional built
 rem 0.0.3 2021-11-08T22:05Z Start blending VCbind on-ramp and guard rails
